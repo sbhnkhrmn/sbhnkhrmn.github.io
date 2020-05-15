8098b06c9bbcca17d4b0f8ede1770542e65b8999
Sübhan Kahraman <khrmn.sbhn@icloud.com>
2020-05-16 01:53:28 +0300

    versıon

--- main_css/version_check.js
+++ main_css/version_check.js
@@ -1,53 +1,57 @@
 const VERSION_CHECK_SUPPORTED = "iOS %s ile Uyumludur 👍";
 const VERSION_CHECK_UNSUPPORTED = "⚠ iOS %s -> %s Uyumlu ⚠";
 
-(function(document){
+(function(document) {
 
-"use strict";
+    "use strict";
 
-function parseVersionString(version){
+    function parseVersionString(version) {
 
-var bits=version.split(".");
-return[bits[0],bits[1]?bits[1]:0,bits[2]?bits[2]:0];}
+        var bits = version.split(".");
+        return [bits[0], bits[1] ? bits[1] : 0, bits[2] ? bits[2] : 0];
+    }
 
-function compareVersions(ours,theirs){for(var i=0;i<4;i++){var a=Number(theirs[i]),b=Number(ours[i]);
+    function compareVersions(ours, theirs) {
+        for (var i = 0; i < 4; i++) {
+            var a = Number(theirs[i]),
+                b = Number(ours[i]);
 
-if(a>b){return 1;}
+            if (a > b) { return 1; } else if (a < b) { return -1; }
+        }
+        return 0;
+    }
 
-else if(a<b){return-1;}}
-return 0;}
+    var prerequisite = document.querySelector(".prerequisite"),
+        version = navigator.appVersion.match(/CPU (iPhone )?OS (\d+)_(\d+)(_(\d+))? like/i);
 
-var prerequisite=document.querySelector(".prerequisite"),
-version=navigator.appVersion.match(/CPU (iPhone )?OS (\d+)_(\d+)(_(\d+))? like/i);
+    if (!prerequisite || !version) { return; }
 
-if(!prerequisite||!version){return;}
+    var osVersion = [version[2], version[3], version[5] ? version[5] : 0],
 
-var osVersion=[version[2],version[3],version[5]?version[5]:0],
+        osString = osVersion[0] + "." + osVersion[1] + (osVersion[2] && osVersion[2] != 0 ? "." + osVersion[2] : ""),
 
-osString=osVersion[0]+"."+osVersion[1]+(osVersion[2]&&osVersion[2]!=0?"."+osVersion[2]:""),
+        minString = prerequisite.dataset.minIos,
+        maxString = prerequisite.dataset.maxIos,
+        minVersion = parseVersionString(minString),
+        maxVersion = maxString ? parseVersionString(maxString) : null,
+        message = VERSION_CHECK_SUPPORTED.replace("%s", osString),
+        isBad = false;
 
-minString=prerequisite.dataset.minIos,
-maxString=prerequisite.dataset.maxIos,
-minVersion=parseVersionString(minString),
-maxVersion=maxString?parseVersionString(maxString):null,
-message = VERSION_CHECK_SUPPORTED.replace("%s", osString),
-isBad = false;
+    if (compareVersions(minVersion, osVersion) == -1) {
+        message = VERSION_CHECK_UNSUPPORTED.replace("%s", minString).replace("%s", maxString);
+        isBad = true;
+    } else if (maxVersion != null && compareVersions(osVersion, maxVersion) == -1) {
+        message = VERSION_CHECK_UNSUPPORTED.replace("%s", minString).replace("%s", maxString);
+        isBad = true;
+    }
 
-if(compareVersions(minVersion,osVersion)==-1){
-message = VERSION_CHECK_UNSUPPORTED.replace("%s", minString).replace("%s", maxString);
-isBad = true;}
+    var i = 0;
 
-else if(maxVersion!=null&&compareVersions(osVersion,maxVersion)==-1){
-message = VERSION_CHECK_UNSUPPORTED.replace("%s", minString).replace("%s", maxString);
-isBad = true;}
+    prerequisite.querySelector("p").innerHTML = message;
 
-var i=0;
-
-prerequisite.querySelector("p").innerHTML = message;
-
-	if (isBad) {
-        prerequisite.querySelector("ul").style.backgroundColor = "#DC143C";
+    if (isBad) {
+        prerequisite.querySelector("p").style.backgroundColor = "#DC143C";
     } else {
-        prerequisite.querySelector("ul").style.backgroundColor = "#c1e317";
+        prerequisite.querySelector("p").style.backgroundColor = "#c1e317";
     }
 }(document));
