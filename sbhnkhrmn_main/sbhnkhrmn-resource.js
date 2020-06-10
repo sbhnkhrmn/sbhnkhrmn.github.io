5d9f25d50c15c4928bcc1fd00b4f60ba46caa8aa
Sübhan Kahraman <khrmn.sbhn@icloud.com>
2020-06-10 04:55:24 -0700

    b

--- sbhnkhrmn_main/sbhnkhrmn-resource.js
+++ sbhnkhrmn_main/sbhnkhrmn-resource.js
@@ -43,53 +43,51 @@ $(document).ready(function () {
         $.each(data.ChangelogerTR, function (_key, value) {
           $("#Changeloger").append(value.Changeloger);
         });
-        $.each(data.Page, function (_key, value) {
-          $("#Title").append(value.Title);
-        });
-        return;
+        // return;
       }
-      $.each(data.HTMLEN, function (_key, value) {
-        $("#DescriptionDiv").append(value.Description);
-        $("#Informations").append(value.Informations);
-        $("#Picture").append(value.Picture);
-        $("#Changelog").append(value.Changelog);
-        $("#DebVersion").append(value.DebVersion);
-        $("#Sized").append(value.Sized);
-        $("#IOSVersion").append(value.IOSVersion);
-        $("#Updated").append(value.Updated);
-        $("#AppPRO").append(value.AppPRO);
-        $("#Install").append(value.Install);
-      });
-      $.each(data.EN, function (_key, value) {
-        $("#Package").append(value.Package);
-        $("#Name").append(value.Name);
-        $("#Architecture").append(value.Architecture);
-        $("#Depends").append(value.Depends);
-        $("#Description").append(value.Description);
-        $("#Depiction").append(value.Depiction);
-        $("#Icon")
-          .attr("src", value.Icon)
-          .attr("class", "sk-border-radius sk-app-size zoom");
-        $("#Sileodepiction").append(value.Sileodepiction);
-        $("#Homepage").attr("href", value.Homepage).attr("target", "_blank");
-        $("#Maintainer").append(value.Maintainer);
-        $("#Author").append(value.Author);
-        $("#Section").append(value.Section);
-        $("#Version").append(value.Version);
-        $("#Filename").attr("href", value.Filename).attr("target", "_open");
-        $("#Size").append(value.Size);
-      });
-      $.each(data.ChangelogerEN, function (_key, value) {
-        $("#Changeloger").append(value.Changeloger);
+      else {
+        $.each(data.HTMLEN, function (_key, value) {
+          $("#DescriptionDiv").append(value.Description);
+          $("#Informations").append(value.Informations);
+          $("#Picture").append(value.Picture);
+          $("#Changelog").append(value.Changelog);
+          $("#DebVersion").append(value.DebVersion);
+          $("#Sized").append(value.Sized);
+          $("#IOSVersion").append(value.IOSVersion);
+          $("#Updated").append(value.Updated);
+          $("#AppPRO").append(value.AppPRO);
+          $("#Install").append(value.Install);
+        });
+        $.each(data.EN, function (_key, value) {
+          $("#Package").append(value.Package);
+          $("#Name").append(value.Name);
+          $("#Architecture").append(value.Architecture);
+          $("#Depends").append(value.Depends);
+          $("#Description").append(value.Description);
+          $("#Depiction").append(value.Depiction);
+          $("#Icon")
+            .attr("src", value.Icon)
+            .attr("class", "sk-border-radius sk-app-size zoom");
+          $("#Sileodepiction").append(value.Sileodepiction);
+          $("#Homepage").attr("href", value.Homepage).attr("target", "_blank");
+          $("#Maintainer").append(value.Maintainer);
+          $("#Author").append(value.Author);
+          $("#Section").append(value.Section);
+          $("#Version").append(value.Version);
+          $("#Filename").attr("href", value.Filename).attr("target", "_open");
+          $("#Size").append(value.Size);
+        });
+        $.each(data.ChangelogerEN, function (_key, value) {
+          $("#Changeloger").append(value.Changeloger);
+        });
+      }
+
+      $.each(data.Images, function (_key, value) {
+        $("#LinkImages").attr("href", value.Path).attr("target", "_blank");
+        $("#SourcesImg").attr("src", value.Path).attr("style", "width:40%");
       });
       $.each(data.Page, function (_key, value) {
         $("#Title").append(value.Title);
-      });
-      $.each(data.Images, function (_key, value) {
-        $("#LinkImages").attr("href", value.Path).attr("target", "_blank");
-        $("#SourcesImg")
-        .attr("src", value.Path)
-        .attr("style", "width:40%");
       });
     },
   });
