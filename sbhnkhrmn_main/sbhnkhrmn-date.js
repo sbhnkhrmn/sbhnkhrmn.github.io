9abaa1f2f9cf3332f161639b703ba725c95f273b
Sübhan Kahraman <khrmn.sbhn@icloud.com>
2020-06-10 10:27:59 -0700

    rrr

--- sbhnkhrmn_main/sbhnkhrmn-date.js
+++ sbhnkhrmn_main/sbhnkhrmn-date.js
@@ -1,5 +1,5 @@
 
-const lastModifiedDate = new File([], 'SBHNKHRMN.json').lastModified.toLocaleString("tr-TR", {
+const lastModifiedDate = new File([], 'SBHNKHRMN.json').lastModifiedDate.toLocaleString("tr-TR", {
   weekday: "short",
   month: "long",
   day: "2-digit",
