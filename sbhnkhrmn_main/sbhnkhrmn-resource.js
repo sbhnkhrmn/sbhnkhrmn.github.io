4b82e438c4aa37a1c2aa87c9c17b6c76f234298c
Sübhan Kahraman <khrmn.sbhn@icloud.com>
2020-06-10 08:04:37 -0700

    c

--- sbhnkhrmn_main/sbhnkhrmn-resource.js
+++ sbhnkhrmn_main/sbhnkhrmn-resource.js
@@ -82,13 +82,13 @@ $(document).ready(function () {
         });
       }
 
-      $.each(data.Images, function (_key, value) {
-        var count = value.Path.split(',');
-        debugger;
-        for (let index = 1; index >= count.length; index++) {
-          $('#foto').append('<a href="' + count[index] + '"><img style="width:40%" src="' + count[index] + '"/></a>');             
-        }
-      });
+      // $.each(data.Images, function (_key, value) {
+      //   var count = value.Path.split(',');
+      //   debugger;
+      //   for (let index = 1; index < count.length; index++) {
+      //     $('#foto').append('<a href="' + count[index] + '"><img style="width:40%" src="' + count[index] + '"/></a>');             
+      //   }
+      // });
       $.each(data.Page, function (_key, value) {
         $("#Title").append(value.Title);
       });
