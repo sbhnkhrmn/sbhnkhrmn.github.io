427ab43c1dbcca5661afa445218dfb0478f7e4cd
Sübhan Kahraman <khrmn.sbhn@icloud.com>
2020-06-10 11:08:04 -0700

    sss

--- sbhnkhrmn_main/sbhnkhrmn-date.js
+++ sbhnkhrmn_main/sbhnkhrmn-date.js
@@ -2,3 +2,15 @@
 const jsonFile = window.location.pathname.replace("index.html", "SBHNKHRMN.json");
 const lastModifiedDate = new File([], jsonFile).lastModified
 document.getElementById("date").innerHTML = new Date(lastModifiedDate);
+
+const fileInput = document.querySelector('body');
+fileInput.addEventListener('load', (event) => {
+  // files is a FileList object (similar to NodeList) 
+  const files = event.target.files;
+  debugger;
+
+  for (let file of files) {
+    const date = new Date(file.lastModified);
+    console.log(`${file.name} has a last modified date of ${date}`);
+  }
+});
